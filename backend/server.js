9714227a1299ebf2d8483196728d7d2e89c68d75
Aryan <aryanpandeyair@gmail.com>
2022-06-21 01:42:54 +0530

    2nd commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
 const app = require("./app");
 
+const dotenv = require("dotenv");
 
-app.listen(process.env.PORT,() =>{
-    console.log(`server is runing on http://localhost:${process.env.PORT}`)
+const connectDatabase = require("./config/database");
+
+//config
+
+dotenv.config({path : "backend/config/config.env"}); 
+
+//Connecting  to database
+
+connectDatabase()
+
+
+
+
+
+
+
+app.listen(process.env.PORT,() =>{                                             // creating a server and giving port no.
+    console.log(`server is runing on http://localhost:${process.env.PORT}`)    // call back function
 })
