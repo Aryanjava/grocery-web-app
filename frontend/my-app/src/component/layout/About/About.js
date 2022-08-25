9c95c1fef2d8247ce6d41c32c07d4e4e234943ce
Aryan <aryanpandeyair@gmail.com>
2022-08-25 12:05:15 +0530

    7th commit

---
+++ frontend/my-app/src/component/layout/About/About.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://instagram.com/_aryan_pandey__";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/grocery/image/upload/v1657590994/samples/aryan2_ge3ley.jpg"
+              alt="Founder"
+            />
+            <Typography>Aryan Pandey</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a college project made by Aryan Pandey ,Mahalingesh , Manikanth
+              and Ravi . This MERN based project .
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.linkedin.com/in/aryan-pandey-b81663209"
+              target="blank"
+            >
+              <YouTubeIcon className="linkedinSvgIcon" />
+            </a>
+
+            <a href="https://instagram.com/_aryan_pandey__" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
